45b7d7a9b03db32acaf73f3f129e6adfd0ca837b
= <=>
2021-02-15 02:25:49 +0530

    new chnages

--- story_key.py
+++ story_key.py
@@ -223,6 +223,10 @@ class application_window():
             total_sent = total_sent.replace('. ,',', ')
             total_sent = total_sent.replace('..','. ')
             total_sent = total_sent.replace(',,',', ')
+            total_sent = total_sent.replace(', ,',', ')
+            total_sent = total_sent.replace('. .','. ')
+          
+            total_sent = total_sent.replace('  ',' ')
 
             """
             for rep in self.new_report_choice:
@@ -1158,7 +1162,7 @@ class application_window():
           if back_category_value == self.get_category_value():
         
             
-            if self.remark_sheet[self.idx] == 0:
+            if self.remark_sheet[self.idx]!= 0:
   
               if self.df_attempt[self.idx] == 1:
                 print(row['AN-Data dictionary'])
